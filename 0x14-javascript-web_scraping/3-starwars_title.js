56f2cfa0e6102eff50bc653b99dfb1f4bac9cdb6
Jerethink <jerethink@gmail.com>
2023-04-26 02:38:57 +0000

    push

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,17 @@
 #!/usr/bin/node
+// Prints the title of a Star Wars movie where
+// the episode number matches a given integer.
+
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const argv = process.argv;
+const episodeID = argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + episodeID;
+request(url, (error, response, body) => {
+  if (error === null) {
+    const responseBody = JSON.parse(body);
+    console.log(responseBody.title);
+  } else {
+    console.log(error);
+  }
 });
 
